b6d55e445b410f547d621ff8b0d55f6402540e71
Javier Fernandrez <javierfz1980@gmail.com>
2019-02-02 23:34:26 -0300

    Initial login validation

--- gl-trainning/client/src/components/login/Login.js
+++ gl-trainning/client/src/components/login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
@@ -12,8 +12,81 @@ import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
 import StyledButton from "../../theme/gl-theme/Buttons";
 import {LoginStyles} from "./Login-styles";
+import axios from 'axios';
 
-function Login(props) {
+class Login extends Component {
+
+  state = {
+    loading: false,
+    loggedIn: false,
+    email: '',
+    password: ''
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const payload = {
+      username: this.state.email,
+      password: this.state.password
+    };
+    console.log('try to login...', payload);
+    axios.post('http://localhost:3001/login', payload)
+      .then(response => {
+        if (response.data['token'] && response.data['token'] !== null && response.data['token'] !== undefined) {
+          console.log('login success:', response.data);
+        }
+      })
+      .catch(error => console.log('error:', error))
+  };
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <main className={classes.main}>
+        <CssBaseline />
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} onSubmit={this.handleSubmit}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input id="email" name="email" autoComplete="email" autoFocus value={this.state.email} onChange={this.handleInputChange}/>
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input name="password" type="password" id="password" autoComplete="current-password" value={this.state.password} onChange={this.handleInputChange}/>
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <StyledButton
+              type="submit"
+              fullWidth
+              variant="contained"
+              className={classes.submit}
+            >
+              Sign in
+            </StyledButton>
+          </form>
+        </Paper>
+      </main>
+    );
+  };
+}
+/*const Login = (props) => {
+
   const { classes } = props;
 
   return (
@@ -44,6 +117,7 @@ function Login(props) {
             fullWidth
             variant="contained"
             className={classes.submit}
+            clicked={}
           >
             Sign in
           </StyledButton>
@@ -51,7 +125,7 @@ function Login(props) {
       </Paper>
     </main>
   );
-}
+};*/
 
 
 export default withStyles(LoginStyles)(Login);
