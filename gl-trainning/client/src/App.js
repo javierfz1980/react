1b617be06b19e1d1adff7c3aa42f77723563c54c
Javier Fernandrez <javierfz1980@gmail.com>
2019-02-02 21:57:55 -0300

    Apply custom GL theme to app component and also implements initial routing for login and webapp

--- gl-trainning/client/src/App.js
+++ gl-trainning/client/src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from "./components/login/Login";
+import Route from "react-router/es/Route";
+import {Switch} from "react-router";
+import Webapp from "./components/webapp/Webapp";
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/login" component={Login} />
+          <Route path="/webapp" component={Webapp} />
+        </Switch>
+      </section>
     );
   }
+
 }
 
 export default App;
--- gl-trainning/client/src/index.js
+++ gl-trainning/client/src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import glTheme from "./theme/gl-theme/Theme";
+import MuiThemeProvider from "@material-ui/core/es/styles/MuiThemeProvider";
+import BrowserRouter from "react-router-dom/es/BrowserRouter";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MuiThemeProvider theme={glTheme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </MuiThemeProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
