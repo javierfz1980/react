ef5574d8f4fc18666968f0868f5062b9df321ca6
Javier Fernandrez <javierfz1980@gmail.com>
2019-02-02 21:56:59 -0300

    Login component

---
+++ gl-trainning/client/src/components/login/Login.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+import StyledButton from "../../theme/gl-theme/Buttons";
+import {LoginStyles} from "./Login-styles";
+
+function Login(props) {
+  const { classes } = props;
+
+  return (
+    <main className={classes.main}>
+      <CssBaseline />
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">Email Address</InputLabel>
+            <Input id="email" name="email" autoComplete="email" autoFocus />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input name="password" type="password" id="password" autoComplete="current-password" />
+          </FormControl>
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <StyledButton
+            type="submit"
+            fullWidth
+            variant="contained"
+            className={classes.submit}
+          >
+            Sign in
+          </StyledButton>
+        </form>
+      </Paper>
+    </main>
+  );
+}
+
+
+export default withStyles(LoginStyles)(Login);
